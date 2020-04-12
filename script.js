a6862d0c77964eba8b21cf644931ef2d3ec1c023
Cooper <cooper9040@gmail.com>
2020-04-12 16:10:14 +0800

    Added live grid size slider

--- script.js
+++ script.js
@@ -1,44 +1,52 @@
-let columns = 50;
-let rows = 50;
 let color = "rgb(132, 105, 255)";
-
-console.log(columns);
-
 let pointerDown = false;
 
 const grid = document.querySelector(".grid-container");
-grid.style.gridTemplateColumns =`repeat(${columns}, 1fr)`;
-grid.style.gridTemplateRows =`repeat(${rows}, 1fr)`;
 
-document.addEventListener("mousedown", (e) => 
-{
-    pointerDown = true;
-});
 
-document.addEventListener("mouseup", (e) =>
-{
-    pointerDown = false;
-});
+document.addEventListener("mousedown", (e) => { pointerDown = true; });
+
+document.addEventListener("mouseup", (e) => { pointerDown = false; });
 
 function changeColor(e)
 {
     e.target.style.backgroundColor = color;
 }
 
-for (let i = 0; i < rows; i++) 
+function createGrid(size)
 {
-    for (let j = 0; j < columns; j++) 
+    grid.style.gridTemplateColumns =`repeat(${size}, 1fr)`;
+    grid.style.gridTemplateRows =`repeat(${size}, 1fr)`;
+
+    for (let i = 0; i < size; i++) 
     {
-        let newDiv = document.createElement("div");
+        for (let j = 0; j < size; j++) 
+        {
+            let newDiv = document.createElement("div");
 
-        newDiv.addEventListener("mouseover", (e) => {if(pointerDown) changeColor(e); });
+            newDiv.addEventListener("mouseover", (e) => {if(pointerDown) changeColor(e); });
 
-        newDiv.addEventListener("click", changeColor);
-      
-        grid.appendChild(newDiv);
+            newDiv.addEventListener("click", changeColor);
+        
+            grid.appendChild(newDiv);
+        }
+        
     }
-    
 }
+
+function deleteGrid()
+{
+    grid.innerHTML = "";
+}
+
+createGrid(50);
+
+const gridSlider = document.getElementById("gridRange");
+gridSlider.addEventListener("input", (e) => 
+{
+    deleteGrid();
+    createGrid(e.target.value);
+});
 
 
 
